8e8878db13a6da6f9d9cbde4dd59ae5bdfc4d13b
almechalov <97981506+almechalov@users.noreply.github.com>
2022-05-31 20:51:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/mk_MK/sdk.js#xfbml=1&version=v13.0" nonce="7x4lAUMZ"></script>
+<script src="/js/bootstrap.min.js"></script>
+
+</head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC">
+<link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
+
+
+
+<link href="style.css" rel="stylesheet">
+<body>
+ 
+    <?php include('header.php')?>
+
+   <main>
+   <div class="home-more-stuff">
+        <div class="more-stuff-grid">
+          <img
+            src="/img/wings.jpg" alt="wings" style="height: 400px; width:400px"
+            alt=""
+            class="slide-in from-left"
+          />
+          <p class="slide-in from-right" style="border: none;">
+            You don't need WINGS to fly.<br>
+            Visit our spot to enjoy our delicious meals
+          </p>
+        </div>
+
+        <div class="more-stuff-grid">
+          <p class="slide-in from-left" style="border: none;">
+          We always keep the food standards high when preparing the meals. 
+          </p>
+          <img 
+            src="/img/burgersbig.png"  style="height: 400px ; width: 400px;  border-radius: 50%;"
+            alt=""
+            class="slide-in from-right"
+          />
+        </div>
+        <div class="more-stuff-grid">
+          <img
+            src="/img/grandeps.png" style="height: 400px ; width: 400px ;border-radius: 50%;"
+            alt="burgerps"
+            class="slide-in from-left"
+          />
+          <p class="slide-in from-right" style="border: none;">
+          Grande crispy (crispy chicken steak x2, seasonal salad, mayo, french fries, bacon, yellow cheese, BBQ sauce)
+          </p>
+        </div>
+        <div class="more-stuff-grid">
+          <p class="slide-in from-left" style="border: none;">
+          Combo box (4 tenders, 4 wings, 4 nuggets, french fries x2, 4 onion rings, sauce)
+          </p>
+          <img
+          src="/img/combobox.png" style="height: 400px; width: 400px; border-radius: 50%;"
+            alt="combobox"
+            class="slide-in from-right"
+          />
+        </div>
+        <div class="more-stuff-grid">
+          <img
+            src="/img/triplbejkonsmesh.png" style="height: 400px; width: 400px; border-radius: 50%;"
+            alt="triplebacon"
+            class="slide-in from-left"
+          />
+          <p class="slide-in from-right" style="border: none;">
+            Triple bacon smash (3 patties, french fries, bacon, yellow cheese, caramelized onions, toasted bread roll)
+          </p>
+        </div>
+        <div class="more-stuff-grid">
+          <p class="slide-in from-left" style="border: none;">
+         Caesar salad(crispy chicken, romaine lettuce, corn, parmesan cheese, cherry tomatoes, integral bread,dressing, balsamic vinegar)
+          </p>
+          <img
+            src="/img/cezar2.png" style="height: 400px; width: 400px; border-radius: 50%;"
+            alt="cezar"
+            class="slide-in from-right"
+          />
+        </div>
+      </div>
+      <section video-red>
+        <div class="container video">
+            <div class="row vieo">
+               <div class="col-lg-4">
+                 <div class="address">
+                    <i class="bi-geo-alt" style="width: 44px ; height: 44px; color: white;">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25px" color= "white" height="25px" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                         <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                      </svg>
+                       
+                    </i>
+                    <h4 style="padding-left: 60px; color: rgb(138, 31, 31);">Location:</h4>
+                    <p style= "padding-left: 60px; color: #c53636;">General M. Apostolski 2000 Stip </p>
+                 </div>
+                 <div class="open-hours" style="margin-top: 40px;"> 
+                    <i class="bi-clock"  style="width: 44px ; height: 44px; color: white;">
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+                                    <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                                       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                                   </svg>
+                    </i>
+                       <h4>Open Hours:</h4>
+                       <p style="padding-left: 60px; color: #c53636;"> Monday-Saturday:<br>
+                         8:30 AM - 11:30 PM</p>
+                  </div>
+                     <div class="phone" style="margin-top: 40px;"> 
+                         <i class="bi-phone" style="width: 44px ; height: 44px; color: white;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                              <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                            </svg>
+                          </i>
+                            <h4>Call:</h4>
+                             <p style="padding-left: 60px; color: #c53636;">+389 32 605 099</p>
+                      </div>
+              </div>
+                <div class="col-lg-8">
+                   
+                   
+                   <iframe style="border: 4px solid #8a1f1f;" width="500" height="270" src="https://www.youtube.com/embed/BNz7XQGCx8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                
+            </div>
+
+
+        </div>
+        
+        <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%D0%93%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D0%BB%20%D0%9C.%20%D0%90%D0%BF%D0%BE%D1%81%D1%82%D0%BE%D0%BB%D1%81%D0%BA%D0%B8%20(0.60%20mi)%20Stip,%20Republic%20of%20Macedonia,%202000+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/marine-gps/">ship tracker</a></iframe></div>
+      </section>
+   </main>
+
+
+   <script src="observers.js"></script>
+     
+ 
+</body>
+  <footer>
+   <?php include('footer.php'); ?>
+  </footer>
+</html>
